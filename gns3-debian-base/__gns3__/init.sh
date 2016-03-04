40ffe93d543afdea7122e33c044434716d1f74f5
Bernhard Ehlers <be@bernhard-ehlers.de>
2016-03-04 13:35:06 +0100

    gns3-debian-base/__gns3__: remove tasks now done by GNS3 - /etc/hosts is now initialized by GNS3 - interfaces are activated by GNS3 - shell process no longer needed, aux console (docker-exec) implemented in GNS3

--- gns3-debian-base/__gns3__/init.sh
+++ gns3-debian-base/__gns3__/init.sh
@@ -1,30 +1,12 @@
 #!/bin/sh
 # init.sh - GNS3 specific docker initialization
 
-# Wait 2 seconds to settle the network interfaces
-sleep 2
-
-# /etc/hosts
-[ -s /etc/hosts ] || cat > /etc/hosts << __EOF__
-127.0.1.1	$HOSTNAME
-127.0.0.1	localhost
-::1	localhost ip6-localhost ip6-loopback
-fe00::0	ip6-localnet
-ff00::0	ip6-mcastprefix
-ff02::1	ip6-allnodes
-ff02::2	ip6-allrouters
-__EOF__
-
-# configure loopback interface
-ip link set dev lo up
-
 # configure eth interfaces
 sed -n 's/^ *\(eth[0-9]*\):.*/\1/p' < /proc/net/dev | while read dev; do
 	eval ip=\$IP_$dev
 	[ -n "$ip" ] && ip -family inet address add $ip dev $dev
 	eval ip=\$IPv6_$dev
 	[ -n "$ip" ] && ip -family inet6 address add $ip dev $dev
-	ip link set dev $dev up
 done
 
 # IP routes
@@ -46,13 +28,6 @@ for i in "" 0 1 2 3 4 5 6 7 8 9; do
 	dest=${route#* } ; dest=${dest%% *}
 	[ -n "$dest" ] && ip -family inet6 route add $net via $dest
 done
-
-# start shell in background
-echo '(sleep 1; kill $$) &' > /tmp/start_sh
-echo 'bash 2>&1' >> /tmp/start_sh
-sh /tmp/start_sh 2> /dev/null
-rm /tmp/start_sh
-echo
 
 # continue normal docker startup
 exec "$@"
